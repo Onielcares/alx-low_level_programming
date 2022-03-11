95519fe676a0b8a4e4e8249dba2fab8c916877dc
Onielcares <Dehinbonatt@gmail.com>
2022-03-11 10:01:43 -0800

    Else if C Learning

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Determine if last string is greater than 5, lesser than 6, or is 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n, l;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	l = n % 10;
+	if (l > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, l);
+	}
+	else if (l == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, l);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6\n", n, l);
+	}
+	return (0);
+}
